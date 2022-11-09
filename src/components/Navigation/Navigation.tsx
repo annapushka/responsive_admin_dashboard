7e888e44d2bf0ed31cc390507836d798e9edbc40
Anna Pushkareva <smirnova_spb@mail.ru>
2022-11-09 14:48:06 +0300

    first commit

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,15 @@
+
+import * as React from 'react';
+import './Navigation.scss';
+
+
+type Props = {
+
+};
+export const Navigation = (props: Props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
