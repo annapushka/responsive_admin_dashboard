9580f037dd704bcea026e0d86a63f33b987b1e87
Anna Pushkareva <smirnova_spb@mail.ru>
2023-01-31 15:17:50 +0300

    react-spline added

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+import './Avatar.scss';
+
+const Avatar = () => {
+
+    return (
+        <div className='avatar'>
+            <Spline scene="https://prod.spline.design/fds1X4AyzjeV30PC/scene.splinecode" />
+        </div>
+    )
+}
+
+export default Avatar;
