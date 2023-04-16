7cd169e6351568c772f0d18c3df59c126a761b29
Anna Pushkareva <smirnova_spb@mail.ru>
2023-04-16 11:09:01 +0300

    add test for App mount

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,2 +1,9 @@
-import {test, expect} from '@playwright/experimental-ct-react';
+import { test, expect } from '@playwright/experimental-ct-react';
 
+import App from './App';
+
+test('First test of component', async ({ mount }) => {
+
+    const component = await mount(<App />);
+    await expect(component).toHaveId('root')
+})
